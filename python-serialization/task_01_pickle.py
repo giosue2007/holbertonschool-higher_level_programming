3bf0757357944035da1e6bcc76f4308cd942b986
giosue2007 <toxicgiosue@gmail>
2026-05-28 09:03:11 +0200

    add perm and c

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
-"""Module that defines CustomObject class."""
 import pickle
 
 
 class CustomObject:
-    """Class that defines a custom object."""
-
     def __init__(self, name, age, is_student):
-        """Initialize CustomObject."""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Display object attributes."""
         print("Name: {}".format(self.name))
         print("Age: {}".format(self.age))
         print("Is Student: {}".format(self.is_student))
 
     def serialize(self, filename):
-        """Serialize object to a file."""
         try:
             with open(filename, "wb") as f:
                 pickle.dump(self, f)
@@ -28,7 +22,6 @@ class CustomObject:
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize object from a file."""
         try:
             with open(filename, "rb") as f:
                 return pickle.load(f)
