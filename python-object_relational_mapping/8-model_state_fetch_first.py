b53695e8d78a1c1fafc2b58d3552272de7bc6c55
giosue2007 <toxicgiosue@gmail>
2026-06-09 15:26:14 +0200

    add perm and c

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-"""Prints the first State object from the database."""
+"""
+This module prints the first State object from the database hbtn_0e_6_usa
+using SQLAlchemy.
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    # Connexion à la base de données MySQL
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
+
+    # Ouverture de la session ORM
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Requête ORM pour récupérer uniquement le premier État par son ID
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Affichage du résultat selon s'il existe ou non
+    if first_state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(first_state.id, first_state.name))
+
+    # Fermeture de la session
     session.close()
