a140ffa8a0eb56bad7c1ae23ec0846ed16f17eb8
giosue2007 <toxicgiosue@gmail>
2026-06-09 14:46:26 +0200

    add perm and c

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all cities of a state given as argument from the database hbtn_0e_4_usa
+This module provides a script that takes in the name of a state
+as an argument and lists all cities of that state, using the
+database hbtn_0e_4_usa.
 """
 import sys
 import MySQLdb
