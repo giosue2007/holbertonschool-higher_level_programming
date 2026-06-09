cd27e52572a4d87a37d8abf51153f751cc957438
giosue2007 <toxicgiosue@gmail>
2026-06-09 14:57:00 +0200

    add perm and c

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
-This module defines a State class and an instance of Base
-to work with SQLAlchemy ORM.
+This module lists all State objects from the database hbtn_0e_6_usa
+using SQLAlchemy.
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
 
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
 
-class State(Base):
-    """
-    State class inherits from Base and links to the MySQL table states.
-    """
-    __tablename__ = 'states'
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
