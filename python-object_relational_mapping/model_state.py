04aa095dd0cd43be142e43eb7e62080278b05606
giosue2007 <toxicgiosue@gmail>
2026-06-09 14:48:55 +0200

    add perm and c

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a State class and an instance of Base
+to work with SQLAlchemy ORM.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Création de l'instance de Base pour la liaison ORM
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class inherits from Base and links to the MySQL table states.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
