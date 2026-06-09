09843f2d5139fc08cb63164907d2661967ac0f3c
giosue2007 <toxicgiosue@gmail>
2026-06-09 14:55:25 +0200

    add perm and c

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 """
-This module lists all State objects from the database hbtn_0e_6_usa
-using SQLAlchemy.
+This module defines a State class and an instance of Base
+to work with SQLAlchemy ORM.
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-if __name__ == "__main__":
-    # Configuration du moteur de connexion à MySQL
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
+class State(Base):
+    """
+    State class inherits from Base and links to the MySQL table states.
+    """
+    __tablename__ = 'states'
 
-    # Création de la session pour interagir avec la base de données
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Récupération de tous les États, triés par ID de manière croissante
-    states = session.query(State).order_by(State.id).all()
-
-    # Affichage strict au format "id: name" demandé par le sujet
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    # Fermeture propre de la session
-    session.close()
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
