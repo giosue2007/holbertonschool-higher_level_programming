444e9fb3c78775631d3b01d0469d5fb6a60dc717
giosue2007 <toxicgiosue@gmail.com>
2026-09-23 15:08:45 +0200

    add perm and c

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """Sérialise un dictionnaire Python vers un fichier XML."""
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+
+def deserialize_from_xml(filename):
+    """Désérialise un fichier XML vers un dictionnaire Python."""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    result_dict = {}
+    for child in root:
+        result_dict[child.tag] = child.text
+
+    return result_dict
